d3c85fa46f0fb54b1ab591b0abb08973e0499e30
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-07-18 21:12:25 -0400

    Working on auth with firebase

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from "react-router-dom";
 
-function Root() {
-  return (
-    <div>
-      ROOT
-    </div>
+import Login from './_Components/LogIn';
+import Register from './_Components/Register';
+import Private from './_Components/Private';
+
+import { fireDB } from './_Firebase/firebase';
+
+
+class Root extends Component {
+
+  state = {
+    loading: false,
+    user: null,
+    loggedIn: false
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fireDB.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        console.log(this.props.history, 'IS THIS A THING');
+        this.setState({ user: user, loggedIn: true });
+        localStorage.setItem('user', user.uid);
+        this.props.history.push('/private');    
+
+
+        console.log('LOGGEDIN')
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+        this.props.history.push('/');    
+
+
+      }
+    });
+  }
+
+  logOutUser = () => {
+    fireDB.auth().signOut();
+    this.setState({ loggedIn: false });
+    console.log('LOGGED OUT');
+  }
+
+  componentWillUnmount() {
+    this.authListener();
+  }
+
+  render() {
+    return (   
+      <React.Fragment>
+      
+      { this.state.loggedIn && <button onClick={this.logOutUser}>log out</button> }
+
+
+      <Switch>
+
+        <Route 
+          exact path='/' 
+          render={(props) => <Login {...props} 
+            user={this.state.user} 
+            loggedIn={this.state.loggedIn}
+            />} 
+        />
+        <Route path='/register' component={Register} />
+
+        <Route render={ (props) => ( this.state.user 
+            ? <Private {...props} /> 
+            : <Redirect to='/login' />
+
+        )} 
+        />
+
+      </Switch>
+      </React.Fragment>   
+    );
+  }
+  
+}
+/*
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+  return(
+
+    <Route {...rest} render={ (props) => ( 
+
+        true === true
+        ? <Component {...props} /> 
+        : <Redirect to='/login' />
+
+      )} 
+    />
+
   );
+  
 }
 
+        <PrivateRoute path='/private' component={Private}/>
+
+*/
 export default Root;
+
+/*
+<ul>
+<li>
+  <Link to="/public">Public Page</Link>
+</li>
+<li>
+  <Link to="/private">Protected Page</Link>
+</li>
+</ul>
+*/
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+
 import './index.css';
 
 import Root from './Root';
 
 
+const RootWithAuth = withRouter(Root);
 
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+  
+  <Router>
+    <RootWithAuth />
+  </Router>, 
+  
+document.getElementById('root'));
